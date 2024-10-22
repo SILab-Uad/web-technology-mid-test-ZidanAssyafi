78bf407c844a07dce3c4ef72658c1989b238fc36
Muhammad Zidan Assyafi Saputra <2311016058@webmail.uad.ac.id>
2024-10-22 11:19:22 +0700

    Attempt 1 - 14.15

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 // TODO: Implement the password generation logic based on user input
-
 const generatePassword = (length, options) => {
     // Character sets for password generation
     const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowercase = "abcdefghijklmnopqrstuvwxyz";
     const numbers = "0123456789";
-    const specialChars = "!@#$%^&*()";
+    const specialChars = "!@#$%^&*()_+-/,.";
 
-    // TODO: Create a variable for the character set based on selected options
+    // TODO: Buat variabel untuk kumpulan karakter berdasarkan opsi yang dipilih
+    let availablechar = '';
+    if (options.includeUppercase) availablechar += uppercase;
+    if (options.includeLowercase) availablechar += lowercase;
+    if (options.includeNumbers) availablechar += numbers;
+    if (options.includeSpecialChars) availablechar += specialChars;
 
-    // TODO: Generate the password based on the selected criteria
+    if (availablechar === '') {
+        return 'Pilih minimal satu jenis karakter!!';
+    }
+
+     // TODO: Hasilkan kata sandi berdasarkan kriteria yang dipilih
+    let password = '';
+    for (let i = 0; i < length; i++){
+        const randomindex = Math.floor(Math.random() * availablechar.length);
+        password += availablechar[randomindex];
+    }
+
     return password;
 };
 
-// TODO: Add event listener to the button to call generatePassword and display the output
+// TODO: Tambahkan pendengar acara ke tombol untuk memanggil generatePassword dan menampilkan hasilnya
 
-// BONUS: Implement the copy to clipboard functionality
+// BONUS: Terapkan fungsionalitas salin ke papan klip
